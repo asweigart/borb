de695bcb97f62508ba2cf4c51442724bddb5d1ae
Al Sweigart <asweigart@gmail.com>
2022-10-17 14:58:12 -0500

    Typo fix for importing ColorExtraction
    Previously color_spectrum_extraction was renamed to color_extraction,
    but the import statement in test_extract_colors.py was not updated with
    the new name. This fix imports the module with the correct name.

--- tests/pdf/canvas/color/test_extract_colors.py
+++ tests/pdf/canvas/color/test_extract_colors.py
@@ -19,7 +19,7 @@ from borb.pdf.canvas.line_art.line_art_factory import LineArtFactory
 from borb.pdf.document.document import Document
 from borb.pdf.page.page import Page
 from borb.pdf.pdf import PDF
-from borb.toolkit.color.color_spectrum_extraction import ColorExtraction
+from borb.toolkit.color.color_extraction import ColorExtraction
 from tests.test_util import compare_visually_to_ground_truth, check_pdf_using_validator
 
 unittest.TestLoader.sortTestMethodsUsing = None
@@ -72,9 +72,9 @@ class TestExtractColors(unittest.TestCase):
             layout.add(
                 Paragraph(
                     """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             """,
                     font_size=Decimal(10),
